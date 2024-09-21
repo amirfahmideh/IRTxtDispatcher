9c56acfdd529c2b3ec03df743a40007eab9d760e
Amir Fahmideh <amir.fahmideh@gmail.com>
2024-09-21 11:26:41 +0330

    update irTxt dispatcher

---
+++ IRTxtDispatcher.Test/NeginTests.cs
@@ -0,0 +1,15 @@
+namespace IRTxtDispatcher.Test;
+using IRTxtDispatcher;
+public class NeginTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+
+    }
+}
--- IRTxtDispatcher/IRTxtDispatcherOperation.cs
+++ IRTxtDispatcher/IRTxtDispatcherOperation.cs
@@ -1,6 +1,20 @@
-﻿namespace IRTxtDispatcher;
+﻿using IRTxtDispatcher.DTO;
+using IRTxtDispatcher.Interface;
+using PaymentTerminalManager.Factory;
+
+namespace IRTxtDispatcher;
 
 public class IRTxtDispatcherOperation
 {
-
+    private readonly SendConfiguration configuration;
+    private readonly IOperation operation;
+    public IRTxtDispatcherOperation(SupportedImplementationType _type, SendConfiguration _configuration)
+    {
+        configuration = _configuration;
+        operation = OperationFactory.CreateOperation(_type);
+    }
+    public async Task<List<SendResult>> SendAsync(List<Send> messages)
+    {
+        return await operation.SendAsync(configuration, messages);
+    }
 }
